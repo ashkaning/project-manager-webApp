7518893815d5432bd29c5a88831575b8b70f655e
ashkan ramezani <ashkan.ram70@gmail.com>
2020-05-29 15:38:57 -0700

    added a few codes and some pages are ready to use

---
+++ client/build/precache-manifest.667f2503179e27bd6f5d7ae944a7ceaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3493640c1f62bdb488a8",
+    "url": "/static/css/main.d7c8e2be.chunk.css"
+  },
+  {
+    "revision": "3493640c1f62bdb488a8",
+    "url": "/static/js/main.dd7e734b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "85f96657e0bee441735c",
+    "url": "/static/css/2.fde3a582.chunk.css"
+  },
+  {
+    "revision": "85f96657e0bee441735c",
+    "url": "/static/js/2.57f7f400.chunk.js"
+  },
+  {
+    "revision": "b24a2c9ea3e70599ada160da61692aa2",
+    "url": "/index.html"
+  }
+];

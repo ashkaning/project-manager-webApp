c112c4aa88e942de90888c35260392b8e6fb7926
ashkan ramezani <ashkan.ram70@gmail.com>
2020-05-29 19:21:58 -0700

    fixed a misspelled

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 const role = require("./role");
 const user = require("./user");
 const service = require("./service");
-const getOnerole = require("./getOneRole");
+const getOneRole = require("./getOneRole");
 ////////////////////////////////////
 router.use("/role", role);
 router.use("/user", user);
 router.use("/service", service);
-router.use("/getOnerole", getOnerole);
+router.use("/getOneRole", getOneRole);
 
 module.exports = router;
